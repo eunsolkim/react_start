9fcd547ef3e703c21ab7c52dffa385800f2d4958
eunsolkim <30309750+eunsolkim@users.noreply.github.com>
2022-07-18 20:43:03 +0900

    feat: Implemented chapter4

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from './chp_03/Library'
+import Clock from './chp_04/Clock'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+setInterval(() => {
+  root.render(
   <React.StrictMode>
-    <Library />
+    <Clock />
   </React.StrictMode>
-);
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
