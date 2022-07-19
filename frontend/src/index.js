06f0c696b557e3a6dbaba4014ef132932875ee5c
eunsolkim <30309750+eunsolkim@users.noreply.github.com>
2022-07-19 12:26:44 +0900

    feat: Implemented chpater5

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,15 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from './chp_03/Library'
 import Clock from './chp_04/Clock'
+import CommentList from './chp_05/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval(() => {
-  root.render(
+root.render(
   <React.StrictMode>
-    <Clock />
+    <CommentList />
   </React.StrictMode>
-  );
-}, 1000);
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
