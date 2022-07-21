070146578d75c6dd551b5473f3b18421480d0645
eunsolkim <30309750+eunsolkim@users.noreply.github.com>
2022-07-21 11:23:38 +0900

    feat: Implemented chpater9

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,13 +9,14 @@ import CommentList from './chp_05/CommentList';
 import NotificationList from './chp_06/NotificationList'
 import Accommodate from './chp_07/Accommodate'
 import ConfirmButton from './chp_08/ConfirmButton';
+import LandingPage from './chp_09/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // npx create-react-app으로 프로젝트 생성 시, 자동 설정된 Strict Mode 때문에 
   // react rendering이 두 번 발생 => 주석 처리
   //<React.StrictMode>
-    <ConfirmButton />
+    <LandingPage />
   //</React.StrictMode>
 );
 
